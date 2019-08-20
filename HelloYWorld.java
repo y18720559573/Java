6d194d4d158fd448d56ad7b382d7d15902e83660
y18720559573 <53397796+y18720559573@users.noreply.github.com>
2019-08-20 22:57:33 +0800

    java_test
    java_test

---
+++ HelloYWorld.java
@@ -0,0 +1,13 @@
+public class HelloYWorld {
+	
+	public static void main(String[] args) {
+		int var1 = 1;
+		int var2 = 2;
+		int result = minus(var1, var2);
+		System.out.println("var1 + var2 = " + result);
+	}
+	
+	public static int minus(int var1, int var2) {
+		return (var1 - var2);
+	}
+}
---
+++ example.java
@@ -0,0 +1,13 @@
+public class example {
+	public static void main(String args[]) {   
+		int x = 1;
+		int y = 20;
+		int you = 1;
+		for (x=1; x<=y; x++) {
+			you = you + y;
+		}
+
+		String str = null;
+		System.out.println("length = " + str.length());
+	}
+}
